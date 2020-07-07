4bef52c4ac210420813a000fa8a2b4b52e85cbf6
Eugene White <eugenewhite1994@gmail.com>
2020-07-07 01:23:32 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -5,13 +5,4 @@ def parrot(phrase = "Squawk!")
   puts "Pretty bird!"
 end
 
-parrot
-
-# 1) #parrot should return the default phrase, "Squawk!" when called without any arguments
-
-#   2) #parrot should output the given phrase when called with an argument
-#     Failure/Error: parrot("Pretty bird!")
-
-
-#   3) #parrot should return the given phrase when called with an argument
-#     Failure/Error: expect(phrase).to eq("Pretty bird!")
+parrot(phrase)
